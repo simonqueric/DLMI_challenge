b2de12d37c005af9224e9e7e855f4a3158fd5162
simon <simon.queric@gmail.com>
2024-03-19 22:09:02 +0100

    add datasets

--- datasets.py
+++ datasets.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
 import torch
+from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
 from datetime import datetime
 import glob
 
+mean = [0.84, 0.72, 0.71]
+std = [0.19, 0.21,  0.09]
 class ClinicalData(Dataset):
     def __init__(self, data):
         if not isinstance(data, pd.DataFrame):
@@ -27,33 +32,7 @@ class ClinicalData(Dataset):
         
         return x, torch.Tensor([y])
     
-    
 class ImageDataset(Dataset):
-    def __init__(self, data, root_dir):
-        if not isinstance(data, pd.DataFrame):
-            raise Exception("data shoud be a pandas.DataFrame")
-        if not isinstance(root_dir, str):
-            raise Exception("root_dir shoud be a string")
-            
-        self.id_label = {}
-        for Id, label in zip(data["ID"], data["LABEL"]):
-            self.id_label[Id] = label
-        
-        self.images = []
-        for Id in data["ID"] :
-            self.images += glob.glob(root_dir+"/"+Id+"/*")
-        
-    def __len__(self):
-        return len(self.images)
-    
-    def __getitem__(self, idx):
-        file_name = self.images[idx]
-        sp = file_name.split("/")
-        Id = sp[1] 
-        image, y = plt.imread(file_name), self.id_label[Id]
-        return torch.Tensor(image.transpose(-1, 0, 1)), torch.Tensor([y])
-
-class PatientDataset(Dataset):
     def __init__(self, data, root_dir):
         if not isinstance(data, pd.DataFrame):
             raise Exception("data shoud be a pandas.DataFrame")
@@ -69,12 +48,48 @@ class PatientDataset(Dataset):
     
     def __getitem__(self, idx):
         ID, y = self.id[idx], self.label[idx]
-
         files = glob.glob(self.dir + "/" + ID + "/*")
         b = len(files)
         X = torch.zeros((b, 3, 224, 224))
         for i in range(b):
-            image = plt.imread(files[i])
+            image = Image.open(files[i])
+            image = np.array(image)
+            image = image / 255 
             X[i,:,:,:] = torch.Tensor(image.transpose(-1, 0, 1))
-
         return X, torch.Tensor([y])
+
+transform = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+class PatientDataset(Dataset):
+    def __init__(self, data, root_dir):
+        if not isinstance(data, pd.DataFrame):
+            raise Exception("data shoud be a pandas.DataFrame")
+        if not isinstance(root_dir, str):
+            raise Exception("root_dir shoud be a string")
+            
+        self.id = data["ID"].values        
+        self.label = data["LABEL"].values 
+        self.dir = root_dir
+
+        self.counts = data["LYMPH_COUNT"].values
+        self.ages = []
+        for bod in data["DOB"]:
+            self.ages.append(2024 - int(bod[-4:]))
+        
+        self.features = torch.Tensor(list(zip(self.counts, self.ages)))
+
+    def __len__(self):
+        return len(self.id)
+    
+    def __getitem__(self, idx):
+        ID, y = self.id[idx], self.label[idx]
+        x = self.features[idx]
+        files = glob.glob(self.dir + "/" + ID + "/*")
+        b = len(files)
+        X = torch.zeros((b, 3, 224, 224))
+        for i in range(b):
+            image = Image.open(files[i])
+            image = np.array(image)
+            image = image / 255 
+            X[i,:,:,:] = torch.Tensor(image.transpose(-1, 0, 1))
+        return (X, x), torch.Tensor([y])
