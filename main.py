366d6ff180814f38db47218548f81ff792a4c9b2
simon <simon.queric@gmail.com>
2024-03-23 22:03:48 +0100

    update main

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import balanced_accuracy_score, accuracy_score
+from sklearn.metrics import balanced_accuracy_score, accuracy_score, roc_curve, auc
+from sklearn.model_selection import KFold
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -18,24 +19,97 @@ from datasets import *
 from resnet import *
 import argparse
 
+def train(epoch, print_every, name, model, optimizer, criterion, trainloader, valloader, scheduler=None, save=True) :
+    if epoch % print_every == 0 :
+        print('\nEpoch: %d' % epoch)
+    
 
+    # if "resnet" in name and epoch==20:
+    #     optimizerCNN = torch.optim.SGD(params = model.parameters(), lr=parameters.lr_CNN, momentum=0.9) 
+    global best_acc
+    global loss_train # list storing the loss evolution
+    global loss_val
+    model.train()
+    total_loss = 0
+    for i, (x, y) in enumerate(trainloader) :
+        if "resnet" or "efficientnet" in name:
+            image, clinical = x 
+            clinical = clinical.to(device)
+            image = image.squeeze(0).to(device)
+            y = y.squeeze(0).to(device)
+            optimizer.zero_grad()
+            y_pred = model(image, clinical)
+            loss = criterion(y_pred, y)
+            total_loss += loss.item()
+            loss.backward()
+            optimizer.step()
+        else : 
+            x = x.to(device)
+            y = y.to(device)
+            optimizer.zero_grad()
+            y_pred = model(x)
+            loss = criterion(y_pred, y)
+            total_loss += loss.item()
+            loss.backward()
+            optimizer.step()
+    if scheduler is not None : 
+        scheduler.step()
 
+    model.eval()
+    total_loss /= i+1   
+    loss_train.append(total_loss)
+    if epoch % print_every == 0:
+        print("Loss on training set : {:.3f}".format(total_loss))
+
+    total_loss = 0
+    predictions = []
+    true_labels = []
+
+    with torch.no_grad() :
+        for i, (x, y) in enumerate(valloader) :
+            if "resnet" in name:
+                image, clinical = x 
+                clinical = clinical.to(device)
+                image = image.squeeze(0).to(device)
+                y = y.squeeze(0).to(device)
+                y_pred = model(image, clinical)
+                loss = criterion(y_pred, y)
+                total_loss += loss.item()
+            else : 
+                x = x.to(device)
+                y = y.to(device)
+                y_pred = model(x)
+                loss = criterion(y_pred, y)
+                total_loss += loss.item()
+            predictions += list((y_pred>0.5).detach().cpu().numpy().astype(float))
+            true_labels += list(y.cpu().numpy())
+        balanced_acc = balanced_accuracy_score(true_labels, predictions)      
+    
+    total_loss /= i+1   
+    loss_val.append(total_loss)
+    if epoch % print_every == 0 :
+        print('\nEpoch: %d' % epoch, 'finished')
+        print("Loss on validation set : {:.3f}".format(total_loss))
+        print("balanced accuracy : {:.2f}".format(balanced_acc))
+
+    if balanced_acc >= min(best_acc, 0.85) : 
+        print("Accuracy improved")
+        if save :
+            torch.save(model.state_dict(), "checkpoints/" + name  + "_" + str(balanced_acc)[:4]  +  "_" + str(epoch) + ".pth")
+        if balanced_acc > best_acc :
+            best_acc = balanced_acc
 
 ## DEFAULT PARAMETERS ##
 
-lr_CNN = 1e-3 # 1e-4, 5e-4, 1e-3
-epochs_CNN = 10
-lr_MLP = 1e-1
-epochs_MLP = 1000
-device = "cuda:1"
+lr_CNN = 1e-4 #5e-4 # 1e-4, 5e-4, 1e-3
+epochs_CNN = 30
+device = "cuda:2"
 
 ## Config ##
 
 config = argparse.ArgumentParser(description="Configuration")
 config.add_argument("--lr_CNN", type=float, default=lr_CNN)
-config.add_argument("--lr_MLP", type=float, default=lr_MLP)
 config.add_argument("--epochs_CNN", type=int, default=epochs_CNN)
-config.add_argument("--epochs_MLP", type=int, default=epochs_MLP)
 config.add_argument("--device", type=str, default=device)
 
 parameters = config.parse_args()
@@ -56,143 +130,39 @@ data = pd.read_csv("clinical_annotation.csv")
 data_train_val = data[data["LABEL"]!=-1]
 data_test = data[data["LABEL"]==-1]
 
-
+# print(data_train_val.values)
 # Analysis of the data
-positive_label = data[data["LABEL"]==1]["ID"].values
-negative_label = data[data["LABEL"]==0]["ID"].values
 
-nb_positive = 0 
-nb_negative = 0
-for ID in positive_label :
-    nb_positive += len(glob.glob("trainset/"+ID+"/*"))
-for ID in negative_label :
-    nb_negative += len(glob.glob("trainset/"+ID+"/*"))
+def statistics(data):
+    P = len(data[data["LABEL"]==1]["ID"].values)
+    N = len(data[data["LABEL"]==0]["ID"].values)
+    
+    p = P/(P+N) 
+    n = N/(P+N)
+    print("Proportion of negative samples : {:.2f}".format(p))
+    print("Proportion of positive samples : {:.2f}".format(n))
 
-print("negative samples :", nb_negative)
-print("positive samples :", nb_positive)
 
-data_train, data_val = train_test_split(data_train_val, test_size=0.3)  # 0.3 ? 0.4 ?, random_state=42) # test_size = 0.25
 print("Size training set :", len(data_train))
 print("Size validation set :", len(data_val))
 print("Size test set :", len(data_test))
 print("Size test set :", len(glob.glob("testset/*")) - 1)
 
-# print(data_val)
-clinical_train = ClinicalData(data_train)
-clinical_val = ClinicalData(data_val)
 
-#image_train = ImageDataset(data_train, images_dir)
-#image_val = ImageDataset(data_val, images_dir)
+image_train = PatientDataset(data_train.values, images_dir)
+image_val = PatientDataset(data_val.values, images_dir)
 
-image_train = PatientDataset(data_train, images_dir)
-image_val = PatientDataset(data_val, images_dir)
-
-(X, x), y = next(iter(image_train))
-print("img shape :", X.shape)
-print("max, min :", torch.max(X), torch.min(X))
-
-# print("Number of training images :", len(image_train))
-# print("Number of validation images :", len(image_val))
-
-train_loader_MLP = DataLoader(clinical_train, batch_size=512, shuffle=True)
-val_loader_MLP = DataLoader(clinical_val, batch_size=512, shuffle=True)
-
-batch_size = 64
 train_loader_CNN = DataLoader(image_train, batch_size=1, shuffle=True)
 val_loader_CNN = DataLoader(image_val, batch_size=1, shuffle=True)
 
 
-## Rename train_loader_MLP, val_loader_MLP
-## Validation on val_loader_MLP
-## add model.eval(), model.train()
-
-mlp = MLP().to(device)
-K = 16 # 4 8 16 32 64
-
-
-resnet = models.resnet18(pretrained=False) #.to(device)
-#resnet = models.resnet34(pretrained=False) #.to(device)
+resnet = models.resnet18(pretrained=False)
 cnn = ModifiedResNet(resnet).to(device)
 
-#feature_extractor.load_state_dict(torch.load("checkpoints/CNN0.0005_25.pth"))
-
-optimizerMLP = torch.optim.Adam(params = mlp.parameters(), lr=parameters.lr_MLP)
-criterionMLP = nn.BCELoss()
-
-
-optimizerCNN = torch.optim.Adam(params = cnn.parameters(), lr=parameters.lr_CNN)#, betas=(0.9, 0), weight_decay=5e-4)
+# optimizerCNN = torch.optim.Adam(params = cnn.parameters(), lr=parameters.lr_CNN)#
+optimizerCNN = torch.optim.SGD(params = cnn.parameters(), lr=parameters.lr_CNN, momentum=0.9)
 criterionCNN = nn.BCELoss()
-
-# lmbda = lambda epoch: 0.99
-# scheduler = torch.optim.lr_scheduler.MultiplicativeLR(optimizerCNN, lr_lambda=lmbda)
-# Training MLP ##
-n_epochs = parameters.epochs_MLP
-losses = []
-best_loss = np.inf
-
-
-def train(epoch, print_every, name, model, optimizer, criterion, trainloader, valloader) :
-    if epoch % print_every == 0 :
-        print('\nEpoch: %d' % epoch)
-
-    global best_acc
-    model.train()
-    for i, (x, y) in enumerate(trainloader) :
-        
-        if name=="CNN":
-            image, clinical = x 
-            clinical = clinical.to(device)
-            image = image.squeeze(0).to(device)
-            y = y.squeeze(0).to(device)
-            optimizer.zero_grad()
-            y_pred = model(image, clinical)
-            loss = criterion(y_pred, y)
-            loss.backward()
-            optimizer.step()
-        else : 
-            x = x.to(device)
-            y = y.to(device)
-            optimizer.zero_grad()
-            y_pred = model(x)
-            loss = criterion(y_pred, y)
-            loss.backward()
-            optimizer.step()
-    model.eval()
-    total_loss = 0
-    predictions = []
-    true_labels = []
-
-    with torch.no_grad() :
-        for i, (x, y) in enumerate(valloader) :
-            if name=="CNN":
-                image, clinical = x 
-                clinical = clinical.to(device)
-                image = image.squeeze(0).to(device)
-                y = y.squeeze(0).to(device)
-                y_pred = model(image, clinical)
-            else : 
-                x = x.to(device)
-                y = y.to(device)
-                y_pred = model(x)
-            predictions += list((y_pred>0.5).detach().cpu().numpy().astype(float))
-            true_labels += list(y.cpu().numpy())
-        balanced_acc = balanced_accuracy_score(true_labels, predictions)      
-    
-    if epoch % print_every == 0 :
-        print('\nEpoch: %d' % epoch, 'finished')
-        print("balanced accuracy : {:.2f}".format(balanced_acc))
-
-    if balanced_acc > best_acc : 
-        print("Accuracy improved")
-        torch.save(model.state_dict(), "checkpoints/" + name + "_"+ "epoch_" + str(epoch) +".pth")
-        best_acc = balanced_acc
-
-best_acc = 0
-print_every = 100
-for epoch in tqdm.tqdm(range(n_epochs)):
-   train(epoch, print_every, "MLP", mlp, optimizerMLP, criterionMLP, train_loader_MLP, val_loader_MLP)
-
-
+scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizerCNN, gamma=0.95)
 
 # Training CNN ##
 
@@ -201,70 +171,33 @@ losses = []
 best_loss = np.inf
 best_accuracy = 0
 print_every = 1
-#loss = 0
-# batch_size = 2
-# count = 0
-#Loss = 0
 
-for epoch in tqdm.tqdm(range(n_epochs)):
-   train(epoch, print_every, "CNN", cnn, optimizerCNN, criterionCNN, train_loader_CNN, val_loader_CNN)
+## K fold cross validation ##
 
-for k in tqdm.tqdm(range(n_epochs)):
-    cnn.train()
-    total_loss = 0
-    print("Start Epoch "+str(k))
-    for i, (x, X, y) in tqdm.tqdm(enumerate(train_loader_CNN)) :
-        # count+=1
-        X = X.squeeze(0)
-        y = y.squeeze(0)
-        optimizerCNN.zero_grad()
-        y_pred = cnn(X.to(device), x.to(device))
-        loss = criterionCNN(y_pred, y.to(device)) #/batch_size
-        #Loss+=loss
-        
-        losses.append(loss.item())
-        total_loss+=losses[-1]  
-        loss.backward()
-        optimizerCNN.step()
-        count = 0
-        loss = 0
-    print("Epoch "+str(k)+" finished. \n")
-    print("Loss on Training Set : {:.3f}".format(total_loss/(i+1)))
+k_fold = KFold(n_splits=5, shuffle = True, random_state=42)
+
+print(data_train_val.values.shape)
+
+for i, (train_index, val_index) in enumerate(k_fold.split(data_train_val.values)):
+    train_set = PatientDataset(data_train_val.values[train_index], images_dir)
+    val_set = PatientDataset(data_train_val.values[val_index], images_dir)
+    train_loader = DataLoader(train_set, batch_size=1)
+    val_loader = DataLoader(val_set, batch_size=1)
     
-    print("Validation")
-    total_loss = 0
-    cnn.eval()
-    predictions = []
-    true_labels = []
-    logits = []
-    with torch.no_grad():
-        for i, (x, X, y) in tqdm.tqdm(enumerate(val_loader_CNN)) :
-            X = X.squeeze(0)
-            y = y.squeeze(0)
-            y_pred = cnn(X.to(device), x.to(device))
-            loss = criterionCNN(y_pred, y.to(device))
-            total_loss+=loss.item()
-            logits += list(y_pred.detach().cpu().numpy().astype(float))
-            predictions += list((y_pred>0.5).detach().cpu().numpy().astype(float))
-            true_labels += list(y.cpu().numpy())
+    resnet = models.resnet18(pretrained=False)
+    cnn = ModifiedResNet(resnet).to(device)
+    optimizerCNN = torch.optim.SGD(params = cnn.parameters(), lr=1e-4, momentum=0.9)
+    criterionCNN = nn.BCELoss()
 
-    total_loss /= i+1 
-    print("Loss on Validation set : {:.3f}".format(total_loss))
-    # if total_loss < best_loss :
-    #    print("Loss decreased")
-    #    torch.save(cnn.state_dict(), "checkpoints/CNN" +str(parameters.lr_CNN) +"_" + str(parameters.epochs_CNN) +".pth")
-    #    best_loss = total_loss
-    # print("true labels", true_labels)
-    # print("predictions", predictions)
-    # print("logits", logits)
-    balanced_acc = balanced_accuracy_score(true_labels, predictions)      
-    print("CNN Validation")
-    if balanced_acc > best_accuracy :
-        print("Accuracy improved")
-        torch.save(cnn.state_dict(), "checkpoints/CNN_"  + str(parameters.lr_CNN) + "_"+ str(parameters.epochs_CNN)+".pth")
-        best_accuracy = balanced_acc
-    print("Accuracy score on validation set : {:.2f}".format(accuracy_score(true_labels, predictions)))
-    print("Balanced accuracy score on validation set: {:.2f}".format(balanced_acc))
-    # scheduler.step()
-        
-np.save("training_loss", losses)
+    # lmbda = lambda epoch: 0.99
+    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizerCNN, gamma=0.95)
+    best_accuracy = 0
+    loss_train = []
+    loss_val = []
+    name = "resnet18_fold" + str(i)
+    print(name)
+    for epoch in tqdm.tqdm(range(n_epochs)):
+        train(epoch, print_every, "resnet18_fold" + str(i) , cnn, optimizerCNN, criterionCNN, train_loader, val_loader, scheduler = scheduler, save=True)
+
+    np.save("fold" + str(i) + "_loss_train.npy", loss_train)
+    np.save("fold" + str(i) + "_loss_val.npy", loss_val)
